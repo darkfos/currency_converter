9c2d675da52f5223126de223822dfc69a62c132a
fos <darkfos82@gmail.com>
2023-12-14 02:48:58 +0300

    Добавил апи сервис

--- api/convert_request.py
+++ api/convert_request.py
@@ -1,1 +1,18 @@
 import requests
+import configparser
+
+class Converter:
+
+    def __init__(self):
+        """
+        Инициализация данных
+        """
+
+        parser = configparser.ConfigParser()
+        parser.read('../auth.ini')
+        API_KEY = "".join(filter(lambda word: word not in "\'\"", parser.get('auth', "API_KEY")))
+        SITE = "".join(filter(lambda word: word not in "\'\"", parser.get('auth', "SITE")))
+        data = requests.get(str(SITE)+"".join(list(filter(lambda word: word not in ['\"', "\'"], API_KEY)))).json()
+
+        #Список всех валют
+        self.all_rates = set(data["rates"].keys())
