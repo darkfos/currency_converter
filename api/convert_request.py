dff5b928d783f3984f2308f269ae5451006d98ce
fos <darkfos82@gmail.com>
2023-12-14 14:25:43 +0300

    Рефакторинг кода, добавил метод конвертации

--- api/convert_request.py
+++ api/convert_request.py
@@ -1,6 +1,8 @@
 import requests
 import configparser
 
+from typing import Mapping
+
 class Converter:
 
     def __init__(self):
@@ -10,19 +12,45 @@ class Converter:
 
         parser = configparser.ConfigParser()
         parser.read('../auth.ini')
-        API_KEY = "".join(filter(lambda word: word not in "\'\"", parser.get('auth', "API_KEY")))
-        SITE = "".join(filter(lambda word: word not in "\'\"", parser.get('auth', "SITE")))
-        data = requests.get(str(SITE)+"".join(list(filter(lambda word: word not in ['\"', "\'"], API_KEY)))).json()
+
+        #Данные АПИ сервиса
+        self.__API_KEY = "".join(filter(lambda word: word not in "\'\"", parser.get('auth', "API_KEY")))
+        self.SITE = "".join(filter(lambda word: word not in "\'\"", parser.get('auth', "SITE")))
+
+        self.data = requests.get(self.SITE+"/latest?access_key="+self.__API_KEY).json()
 
         #Список всех валют
-        self.__all_rates = set(data["rates"].keys())
+        self.__all_rates = set(self.data["rates"].keys())
 
-    def get_all_rates(self):
+    def get_all_rates(self) -> list:
         """
         Возвращает список всех валют
         :return:
         """
         return list(self.__all_rates)
 
+    def convert_values(self, type_val_1: str, type_val_2: str, value: Mapping[float, int]) -> Mapping[str, float, int]:
+        """
+        Метод конвертации валют
+        :param type_val_1:
+        :param type_val_2:
+        :param value:
+        :return:
+        """
+
+        try:
+            initial_value = value
+            if type_val_1 != "EUR":
+                initial_value = initial_value / self.data["rates"][type_val_1]
+            result = initial_value * self.data["rates"][type_val_2]
+
+            result = round(initial_value * self.data["rates"][type_val_2], 2)
+
+            return result
+        except:
+            return "Error to convert currencies"
+
+
     def __str__(self):
         return "Class Converter.\nreturn: all_rates, convert currencies"
+
